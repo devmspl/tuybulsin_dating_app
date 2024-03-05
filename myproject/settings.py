c6e195dd55494dae2fd65e94a156190ca9244d0b
dev.mspl <developer.mspl@gmail.com>
2024-03-05 17:17:10 +0530

    setting for cors

--- myproject/settings.py
+++ myproject/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 
@@ -148,3 +149,6 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
