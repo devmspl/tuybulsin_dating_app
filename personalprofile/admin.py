619fe1915f0333c7796f4fbcd5b3e65db0173b50
dev.mspl <developer.mspl@gmail.com>
2024-03-18 18:10:29 +0530

    userpreference

--- personalprofile/admin.py
+++ personalprofile/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PersonalInformation, ImageUpload, Plan
+from .models import PersonalInformation, ImageUpload, Plan, UserPreference
 # Register your models here.
 admin.site.register(PersonalInformation)
 admin.site.register(ImageUpload)
 admin.site.register(Plan)
+admin.site.register(UserPreference)
